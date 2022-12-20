fc94e75ec768be4ebb8262622ca4fdfd0c535362
BFlorian59 <florianbroeks59@gmail.com>
2022-12-20 17:17:51 +0100

    mock

--- src/Service/index.jsx
+++ src/Service/index.jsx
@@ -17,14 +17,14 @@ export default class ComponentDidMount {
          return user
       }else{
          fetch(`http://localhost:3000/user/${userId}`)
-           .then((response) => response.json())
-           .then((data) => {
-              console.log(data);
-              return data;
-           })
-           .catch((err) => {
-              console.log(err.message);
-           });
+         .then((response) => response.json())
+         .then((data) => {
+            console.log(data);
+            return data;
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
       }  
    }
 
@@ -36,14 +36,14 @@ export default class ComponentDidMount {
          return user_18_activity
       }else{
          fetch(`http://localhost:3000//user/${userId}/activity`)
-           .then((response) => response.json())
-           .then((activity) => {
-              console.log(activity);
-              return activity;
-           })
-           .catch((err) => {
-              console.log(err.message);
-           });
+         .then((response) => response.json())
+         .then((activity) => {
+            console.log(activity);
+            return activity;
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
       }  
    }
 
@@ -54,14 +54,14 @@ export default class ComponentDidMount {
          return user_18_sessions
       }else{
          fetch(`http://localhost:3000/user/${userId}/average-sessions`)
-           .then((response) => response.json())
-           .then((session) => {
-              console.log(session);
-              return session;
-           })
-           .catch((err) => {
-              console.log(err.message);
-           });
+         .then((response) => response.json())
+         .then((session) => {
+            console.log(session);
+            return session;
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
       }  
    }
 
@@ -72,49 +72,14 @@ export default class ComponentDidMount {
          return user_18_performance
       }else{
          fetch(`http://localhost:3000/user/${userId}/performance`)
-           .then((response) => response.json())
-           .then((performance) => {
-              console.log(performance);
-              return performance;
-           })
-           .catch((err) => {
-              console.log(err.message);
-           });
+         .then((response) => response.json())
+         .then((performance) => {
+            console.log(performance);
+            return performance;
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
       }  
    }
-
-   //  return data && activity && session && performance !== null?  (
-   //    <div className="posts-container">
-   //       {Object.values(data).map((user) => 
-   //          <div className='titre' key={user.id}>
-   //             <User firstname={user.userInfos.firstName}/>
-   //          </div>  
-   //       )}
-   //       <div className='stat' >
-   //          {Object.values(data).map((user) => 
-   //             <Stat key={user.id} calorieCount={user.keyData.calorieCount} proteinCount={user.keyData.proteinCount}
-   //                carbohydrateCount={user.keyData.carbohydrateCount}  lipidCount={user.keyData.lipidCount} />
-               
-   //          )}
-   //          <div className='graph'>
-   //             {Object.values(activity).map((activitys) => 
-   //                <Activity key={activitys.userId} activity={activitys.sessions} />
-   //             )}
-
-   //             {Object.values(session).map((ses) => 
-   //                <Session key={ses.userId} session={ses.sessions} />
-   //             )}
-
-   //             {Object.values(performance).map((perf) => 
-   //                <Performance key={perf.userId} performance={perf.data} kind={perf.kind} />
-   //             )}
-
-   //             {Object.values(data).map((score) => 
-   //                <Score key={score.id} score={score} />
-   //             )}
-   //          </div> 
-   //       </div>  
-   //    </div>
-      
-   // ):<></>
 }
